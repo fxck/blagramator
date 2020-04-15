27b00d1a3416d30bf3e141a57c42b55bb274e733
Ales Rechtorik <foxxcz@gmail.com>
2020-04-15 17:36:14 +0200

    server, initial work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'b-app',
@@ -6,5 +7,32 @@ import { Component } from '@angular/core';
   styleUrls: [ './app.component.scss' ]
 })
 export class AppComponent {
+
+  title = 'sem nadpis';
+  amount = 5;
+  isVisible = true;
+  list = [ 'polozka jedna', 'polozka dve', 'položka tři' ];
+  person = {
+    name: 'honza',
+    age: 10
+  };
+  listAddControl = new FormControl('naucit se angular', [ Validators.required ]);
+
+  onListAdd() {
+
+    if (this.listAddControl.valid) {
+
+      this.list = [
+        ...this.list,
+        this.listAddControl.value
+      ];
+
+      this.listAddControl.reset();
+
+    } else {
+      alert('tak to seš dement');
+    }
+
+  }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [ AppComponent ],
   imports: [
     BrowserModule,
+    CommonModule,
+    ReactiveFormsModule,
     AppRoutingModule
   ],
   bootstrap: [ AppComponent ]
